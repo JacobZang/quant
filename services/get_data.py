c80d8328141f81cddb199d9b0c7e71d7120e111d
taojinjinjin <1513755499@qq.com>
2024-12-27 22:43:43 +0800

    services: add get_hs_data and model initialization

--- services/get_data.py
+++ services/get_data.py
@@ -21,3 +21,25 @@ def get_shenwan_data():
 	filtered_df = big_df[["指数代码", "指数名称", "发布日期", "市盈率", "市净率"]]
 
 	return filtered_df
+
+def get_hs_data(start, end):
+	big_df = ak.stock_hk_index_daily_sina(symbol="HSI")
+	big_df.rename(
+        columns={
+            "high": "最高价",
+            "low": "最低价",
+            "open": "开盘价",
+            "close": "收盘价",
+			"date": "日期",
+        },
+        inplace=True
+    )
+	big_df['日期'] = pd.to_datetime(big_df['日期'])
+    
+    # 过滤特定时间范围的数据
+	filtered_df = big_df[(big_df['日期'] >= start) & (big_df['日期'] <= end)]
+    
+    # 选择需要的列
+	filtered_df = filtered_df[["日期", "最高价", "最低价", "开盘价", "收盘价"]]
+
+	return filtered_df
